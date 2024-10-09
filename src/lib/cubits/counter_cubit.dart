26c4f7b7ffa213d10d9d709ec59bf276dce7c1c0
zharfan-a <zharfan@walturn.com>
2024-10-09 07:58:01 +0700

    Modify App: zharfan-5 - Success (#2)
    * Update 1 files
    
    * Update 28 files

--- src/lib/cubits/counter_cubit.dart
+++ src/lib/cubits/counter_cubit.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CounterCubit extends Cubit<int> {
 	CounterCubit() : super(0);
 
-	void increment() => emit(state + 1);
+	void increment() => emit(state + 2);
 
 	void decrement() => emit(state - 1);
 }
